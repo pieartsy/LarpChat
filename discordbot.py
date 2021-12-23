45b68cc3697640e6187454e2ff77b924cd33a2f0
pieartsy <17207322+pieartsy@users.noreply.github.com>
2021-12-23 11:50:49 -0500

    serviceable proxy
    makes a proxy for the social media platforms

---
+++ discordbot.py
@@ -0,0 +1,30 @@
+import discord
+from discord.commands import Option
+
+bot = discord.Bot()
+
+def platform_post(platform=str):
+    if platform == "Flitter":
+        colour = 0x55acee
+    elif platform == "XPosure":
+        colour = 0xE1306C
+    elif platform == "Bloggity":
+        colour = 0xffea00
+    return(colour)
+
+@bot.slash_command(guild_ids=[385833475954966529])
+async def post(
+    ctx,
+    platform: Option(str, label="platform", choices=["Bloggity", "Flitter", "XPosure"]),
+    handle: str,
+    makepost: Option(str, label="post")):
+    """Make a social media post"""
+    await ctx.delete()
+    webhook = await ctx.channel.create_webhook(name=platform)
+    colour = platform_post(platform)
+    embed=discord.Embed(description=makepost, title="@" + handle, colour=colour)
+    await webhook.send(embed=embed)
+    await webhook.delete()
+
+
+bot.run("OTIxNjY0NjQ4NjExMjMzODQy.Yb2NFw.CeI7pvyPxdZ9m0uM95XFF3VIpIM")
