b2b758f835133827afaa3420683e3f27d8433c5f
pieartsy <17207322+pieartsy@users.noreply.github.com>
2021-12-26 12:05:53 -0500

    made channels where the webhooks will post
    also added "error message" for flitter

--- discordbot.py
+++ discordbot.py
@@ -4,33 +4,64 @@ from discord.commands import Option
 #to join paths
 import os
 
+from discord.embeds import Embed
+
 bot = discord.Bot()
 
 #discord bot token
 token = os.environ.get('TOKEN')
 
-def platform_post(platform=str):
+guildID = 385833475954966529
+
+def platform_post(platform=str, handle=str, makepost=str):
     if platform == "Flitter":
-        colour = 0x55acee
+        if len(makepost) >= 280:
+            toolong = len(makepost) - 280
+            error = f"Your Flitter post was {toolong} characters too long! Flit shorter!"
+            return(error)
+        else:
+            embed=discord.Embed(description=makepost, title="@" + handle, colour=0x55acee)
     elif platform == "XPosure":
-        colour = 0xE1306C
+        embed=discord.Embed(description=makepost, title="@" + handle, colour=0xE1306C)
     elif platform == "Bloggity":
-        colour = 0xffea00
-    return(colour)
+        embed=discord.Embed(description=makepost, title="@" + handle, colour=0xffea00)
+    return(embed)
+
+@bot.slash_command(guild_ids=[guildID])
+async def channelmaker(
+    ctx,
+    ):
+    """Make channels and webhooks"""
+    platforms = ['Bloggity', 'Flitter', 'XPosure']
+    for platform in platforms:
+        if discord.utils.get(ctx.guild.channels, name=platform.lower()):
+            await ctx.respond("These channels already exist!", ephemeral=True)
+            break
+        else:
+            postChannel = None
+            postChannel = await ctx.guild.create_text_channel(name=platform)
+            await postChannel.set_permissions(ctx.guild.default_role, send_messages=False)
+            await postChannel.create_webhook(name=platform)
 
 @bot.slash_command(guild_ids=[385833475954966529])
 async def post(
     ctx,
-    platform: Option(str, label="platform", choices=["Bloggity", "Flitter", "XPosure"]),
+    platform: Option(str, choices=["Bloggity", "Flitter", "XPosure"]),
     handle: str,
-    makepost: Option(str, label="post")):
+    makepost: Option(str, label="post")
+    ):
     """Make a social media post"""
     await ctx.delete()
-    webhook = await ctx.channel.create_webhook(name=platform)
-    colour = platform_post(platform)
-    embed=discord.Embed(description=makepost, title="@" + handle, colour=colour)
-    await webhook.send(embed=embed)
-    await webhook.delete()
+    content = platform_post(platform, handle, makepost)
+    webhooks = await ctx.guild.webhooks()
+    webhook = discord.utils.get(webhooks, name=platform)
+    if isinstance(content, Embed):
+        await webhook.send(embed=content)
+    else:
+        await ctx.respond(content, ephemeral=True)
 
-
+#@post.error
+#async def post_error(ctx, error):
+ #   if isInstance(error, )
+        
 bot.run(token)
